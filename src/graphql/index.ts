06726ce18859e45e9485fa4b4105dcee5f528279
Nathan Walton <nathan@waltonwebservices.com>
2022-06-14 17:32:24 +0100

    graphql playground running

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,9 +1,10 @@
 import { ApolloServer } from "apollo-server-lambda";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { resolvers } from "./resolvers";
+import PrintsType from "./typeDefs/prints.type";
 
 const server = new ApolloServer({
-  typeDefs: [],
+  typeDefs: [PrintsType],
   resolvers,
   introspection: true,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
--- src/graphql/typeDefs/prints.type.ts
+++ src/graphql/typeDefs/prints.type.ts
@@ -2,50 +2,50 @@ import { gql } from "apollo-server-lambda";
 
 export default gql`
   type Query {
-    getPrints(): Prints
+    getPrints: Prints
   }
 
   type Prints {
-    info: Info;
-    records: [Records];
+    info: Info
+    records: [Records]
   }
 
   type Info {
-    totalrecordsperquery: Int;
-    totalrecords: Int;
-    pages: Int;
-    page: Int;
+    totalrecordsperquery: Int
+    totalrecords: Int
+    pages: Int
+    page: Int
   }
 
   type Records {
-    accessionyear: String;
-    technique: String;
-    mediacount: Int;
-    edition: String;
-    totalpageviews: Int;
-    groupcount: Int;
-    people: [People];
-    objectnumber: String;
-    colorcount: Int;
-    lastupdate: String;
-    rank: Int;
-    imagecount: Int;
-    description: String;
-    dateoflastpageview: String;
-    dateoffirstpageview: String;
-    primaryimageurl: String;
+    accessionyear: String
+    technique: String
+    mediacount: Int
+    edition: String
+    totalpageviews: Int
+    groupcount: Int
+    people: [People]
+    objectnumber: String
+    colorcount: Int
+    lastupdate: String
+    rank: Int
+    imagecount: Int
+    description: String
+    dateoflastpageview: String
+    dateoffirstpageview: String
+    primaryimageurl: String
   }
 
   type People {
-    birthplace: String;
-    name: String;
-    prefix: String;
-    personid: Int;
-    role: String;
-    displayorder: Int;
-    culture: String;
-    displaydate: String;
-    deathplace: String;
+    birthplace: String
+    name: String
+    prefix: String
+    personid: Int
+    role: String
+    displayorder: Int
+    culture: String
+    displaydate: String
+    deathplace: String
     displayname: String
-   }
+  }
 `;
